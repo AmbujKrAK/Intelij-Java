ed9909906dc1007748e0a276bad8018eb165b1f9
Ambuj Kumar <ambuj02dbz@gmail.com>
2025-10-04 18:09:46 +0530

    Adding Function folder

---
+++ Functions/src/StringFun.java
@@ -0,0 +1,11 @@
+public class StringFun {
+    public static void main(String[] args) {
+        String msg = greet();
+        System.out.println(msg);
+    }
+    static String greet(){
+        String greeting = "Hello World";
+        return greeting;
+
+    }
+}
